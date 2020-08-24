015665e7567990573b0cdfc628405f0740adfa8f
Ladislav Kožarin <59497788+everAtWork@users.noreply.github.com>
2020-08-24 14:12:20 +0300

    Updates

---
+++ precache-manifest.8d25ac253ba8dfcf6dda498a05b6b8f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395a5e178428efe8d12deba0537587a5",
+    "url": "/frienz/index.html"
+  },
+  {
+    "revision": "c1f185536d4f2a0c5b82",
+    "url": "/frienz/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b483b1a7443589a52b20",
+    "url": "/frienz/static/css/main.6439ec89.chunk.css"
+  },
+  {
+    "revision": "c1f185536d4f2a0c5b82",
+    "url": "/frienz/static/js/2.3fc42f66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/frienz/static/js/2.3fc42f66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b483b1a7443589a52b20",
+    "url": "/frienz/static/js/main.68b0018c.chunk.js"
+  },
+  {
+    "revision": "7abc0eb8b50eda98d6f8",
+    "url": "/frienz/static/js/runtime-main.d33fdea6.js"
+  }
+]);
